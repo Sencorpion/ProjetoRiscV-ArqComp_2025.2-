2714c6d65f76b9866822ef5e1b52c84723aebdd4
Sencorpion <179764377+Sencorpion@users.noreply.github.com>
2025-11-14 16:30:54 -0300

    JAL and JALR implementation
    Adds support for JAl and JALR functions, enabling unconditional jumps
    and subroutine calls.
    KEY CHANGES:
    1. JAL implementations:
    - Jump logic: A new Jump control signal is generated by the Controller
    to trigger the PC update in the BranchUnit, reusing the existing branch
    hardware (adapting the adder in the BranchUnit module) for unconditional
    jumps.
    - Link logic: MemtoReg control was widened to 2-bits to be able to
    support the write-back of the PC+4 address (It only was able to choose
    the register write data from the ALU or the data memory with one bit).
    With this change, the resmux had to be replaced with a 4-input mux (for
    the new third write-back option).
    
    2. JALR implementations:
    - Jump logic: The ALU is used to calculate the register1 + imm target
    address. A new JumpR control signal is generated by the Controller to
    trigger the PC update in the BranchUnit, also reusing the adder module,
    but utilizing the result (with the least significant bit cleared, as per
    the ISA specification) from the ALU (ALUResult) as the jump target,
    instead of the already calculated PC+IMM.
    - Link logic: Reused logic from the JAL operation.
    
    SUMMARY OF MODULE CHANGES:
    Controller.sv: Decodes the new JAL and JALR opcodes and generates the
    new Jump and Jumpr control signals. Manages new extended MemtoReg
    control logic, updating the RegWrite, ALUSrc, and ALUOp logic.
    imm_Gen.sv: Extended to correctly concatenate the J-type instruction
    immediate (for JAL) and the (new) I-type immediate (for JALR).
    BranchUnit.sv: Logic updated to trigger PC update with Jump and JumpR
    and select the correct target address(PC + IMM for JAL, PC + ALUResult
    for JALR).
    RegPack.sv: Register definitions updated to carry new Jump and JumpR
    signals and the extended MemtoReg signal.
    Datapath.sv: Final mux for writeback replaced (to support new 2-bit
    selector RegtoMem), and new control signals correctly wired through the
    pipeline stages.

--- design/Controller.sv
+++ design/Controller.sv
@@ -9,30 +9,37 @@ module Controller (
     output logic ALUSrc,
     //0: The second ALU operand comes from the second register file output (Read data 2); 
     //1: The second ALU operand is the sign-extended, lower 16 bits of the instruction.
-    output logic MemtoReg,
-    //0: The value fed to the register Write data input comes from the ALU.
-    //1: The value fed to the register Write data input comes from the data memory.
-    output logic RegWrite, //The register on the Write register input is written with the value on the Write data input 
-    output logic MemRead,  //Data memory contents designated by the address input are put on the Read data output
-    output logic MemWrite, //Data memory contents designated by the address input are replaced by the value on the Write data input.
-    output logic [1:0] ALUOp,  //00: LW/SW; 01:Branch; 10: Rtype; 11 Itype
-    output logic Branch  //0: branch is not taken; 1: branch is taken
+    output logic [1:0] MemtoReg,
+    //00: The value fed to the register Write data input comes from the ALU.
+    //01: The value fed to the register Write data input comes from the data memory.
+    //10: The value fed to the register Write data input comes from the Program Counter (plus 4)
+    output logic RegWrite,      // The register on the Write register input is written with the value on the Write data input
+    output logic MemRead,       // Data memory contents designated by the address input are put on the Read data output
+    output logic MemWrite,      // Data memory contents designated by the address input are replaced by the value on the Write data input.
+    output logic [1:0] ALUOp,   // 00: LW/SW; 01:Branch; 10: Rtype; 11 Itype
+    output logic Branch         // 0: branch is not taken; 1: branch is taken
+    output logic Jump           //
+    output logic JumpR          //
 );
 
-  logic [6:0] R_TYPE, LW, SW, BR, IMM;
+  logic [6:0] R_TYPE, LW, SW, BR, IMM, JAL, JALR;
 
-  assign R_TYPE = 7'b0110011;  //add,and,sub,slt,xor,or
-  assign LW = 7'b0000011;  //lw,lh,lb,lbu
-  assign SW = 7'b0100011;  //sw,sh,sbu
-  assign BR = 7'b1100011;  //beq
-  assign IMM = 7'b0010011; // addi, slti, slli, srli, srai
+  assign R_TYPE = 7'b0110011;  // add,and,sub,slt,xor,or
+  assign LW = 7'b0000011;      // lw,lh,lb,lbu
+  assign SW = 7'b0100011;      // sw,sh,sbu
+  assign BR = 7'b1100011;      // beq
+  assign IMM = 7'b0010011;     // addi, slti, slli, srli, srai
+  assign JAL = 7'b1101111;     // jal
+  assign JALR = 7'b1100111;    // jalr
 
-  assign ALUSrc = (Opcode == LW || Opcode == SW || Opcode == IMM);
-  assign MemtoReg = (Opcode == LW);
-  assign RegWrite = (Opcode == R_TYPE || Opcode == LW || Opcode == IMM);
+  assign ALUSrc = (Opcode == LW || Opcode == SW || Opcode == IMM || Opcode == JALR);
+  assign MemtoReg = (Opcode == LW) ? 2'b01 : (Opcode == JAL || Opcode == JALR) ? 2'b10 : 2'b00;
+  assign RegWrite = (Opcode == R_TYPE || Opcode == LW || Opcode == IMM || Opcode == JAL || Opcode == JALR);
   assign MemRead = (Opcode == LW);
   assign MemWrite = (Opcode == SW);
-  assign ALUOp[0] = (Opcode == BR || Opcode == IMM);
-  assign ALUOp[1] = (Opcode == R_TYPE || Opcode == IMM);
+  assign ALUOp[0] = (Opcode == BR || Opcode == IMM || Opcode == JALR);
+  assign ALUOp[1] = (Opcode == R_TYPE || Opcode == IMM || Opcode == JALR);
   assign Branch = (Opcode == BR);
+  assign Jump = (Opcode == JAL);
+  assign JumpR = (Opcode == JALR);
 endmodule
