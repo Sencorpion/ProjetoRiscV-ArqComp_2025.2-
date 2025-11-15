6de124eb466a8bc884d04a0d2f5d5e053a4d5be7
Sencorpion <179764377+Sencorpion@users.noreply.github.com>
2025-11-15 12:28:52 -0300

    Added missing Jump and JumpR wires

--- design/RISC_V.sv
+++ design/RISC_V.sv
@@ -19,7 +19,7 @@ module riscv #(
 );
 
   logic [6:0] opcode;
-  logic ALUSrc, RegWrite, MemRead, MemWrite, Branch;
+  logic ALUSrc, RegWrite, MemRead, MemWrite, Branch, Jump, JumpR;
   logic [1:0] MemtoReg;
   logic [1:0] ALUop;
   logic [1:0] ALUop_Reg;
